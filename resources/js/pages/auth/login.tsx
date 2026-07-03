b655dbe729bd559229f47eb928fa11b91ca3c7d2
Avik Kundu <avik.techwarehut@gmail.com>
2026-07-03 20:08:28 +0530

    Add GitHub Actions deployment 9

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -95,7 +95,7 @@ export default function Login({ status, canResetPassword, demoBusinesses = [] }:
 
     return (
         <AuthLayout
-            title={t("Log in to your accountt")}
+            title={t("Log in to your account")}
             description={t("Enter your credentials to access your account")}
             status={status}
         >
